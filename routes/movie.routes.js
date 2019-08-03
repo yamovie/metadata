4e608198b4cdec9a7aad73d735b4049d87144f44
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-08-03 16:45:57 -0700

    Handle movie routes with movie controller

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const movieController = require('../controllers/movie.controller');
 
-router.get('/', () => {});
-router.get('/:id', () => {});
-router.get('/genre/:id', () => {});
-router.get('/recommend', () => {});
+router.get('/', movieController.readAll);
+router.get('/:id', movieController.readOne);
+router.get('/genre/:id', movieController.readByGenre);
+router.get('/recommend', movieController.readByRecommendation);
+router.get('/search', movieController.readBySearchAll);
+router.get('/search/title', movieController.readBySearchTitle);
+router.get('/search/cast', movieController.readBySearchCast);
+router.get('/search/crew', movieController.readBySearchCast);
 
 module.exports = router;
