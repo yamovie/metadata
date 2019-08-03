56909d7e29094b53db9bba65f2b1003b0c8d674d
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-08-03 12:47:57 -0700

    Configure express

---
+++ config/express.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+// const routes = require('../routes');
+
+/**
+ * Express instance
+ * @public
+ */
+const app = express();
+
+// Request logging
+app.use(morgan('combined'));
+
+// Parse body params and attach them to req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// gzip compression
+app.use(compression());
+
+// Secur apps by setting HTTP headers
+app.use(helmet());
+
+// Enable Cross-Origin Resource Sharing
+app.use(cors());
+
+// Mount api routes
+// app.use('/', routes);
+
+module.exports = app;
