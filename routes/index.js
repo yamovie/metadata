52f2de7b55c90c4f59af7d111302065e16569b23
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-08-03 16:34:43 -0700

    Fix routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const movieRoutes = require('./movie.routes');
 const genreRoutes = require('./genre.routes');
 const providerRoutes = require('./provider.routes');
 
-router.get('/status', (req, res) => res.send('OK'));
-router.get('/movies', movieRoutes);
-router.get('/genres', genreRoutes);
-router.get('/providers', providerRoutes);
+router.use('/status', (req, res) => res.send('OK'));
+router.use('/movies', movieRoutes);
+router.use('/genres', genreRoutes);
+router.use('/providers', providerRoutes);
 
 module.exports = router;
