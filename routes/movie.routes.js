aeb419b873d5acffd645d781b195b42d009a1f24
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-08-03 14:59:31 -0700

    Init movie routes

---
+++ routes/movie.routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.get('/', () => {});
+router.get('/:id', () => {});
+router.get('/genre/:id', () => {});
+router.get('/recommend', () => {});
+
+module.exports = router;
